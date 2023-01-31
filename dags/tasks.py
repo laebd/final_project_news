1e6037a6851809b4238f62270d91d27e789f0c90
laebd <100529557+laebd@users.noreply.github.com>
2023-01-31 13:29:03 +0800

    Update tasks.py

--- dags/tasks.py
+++ dags/tasks.py
@@ -13,5 +13,5 @@ def conn1():
 with DAG(dag_id="parse_source", start_date=datetime(2022, 12, 13), schedule="0 0 * * * *", catchup=False, max_active_runs=1) as dag:
     parse_source = PythonOperator(task_id="parse_source", python_callable = parse_source)    
 
-
+   parse_source
     
